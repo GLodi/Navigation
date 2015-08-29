5cdd0d6735c5d4d4a09d24f4d3ffa8b50d3d050a
GLodi <lodi.giulio@gmail.com>
2015-08-29 22:58:28 +0200

    Implementation of WeatherLib

--- app/src/main/java/com/android4dev/navigationview/FragmentWeather.java
+++ app/src/main/java/com/android4dev/navigationview/FragmentWeather.java
@@ -3,10 +3,20 @@ package com.android4dev.navigationview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.survivingwithandroid.weather.lib.WeatherClient;
+import com.survivingwithandroid.weather.lib.WeatherConfig;
+import com.survivingwithandroid.weather.lib.client.okhttp.WeatherDefaultClient;
+import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
+import com.survivingwithandroid.weather.lib.model.CurrentWeather;
+import com.survivingwithandroid.weather.lib.provider.forecastio.ForecastIOProviderType;
+import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
+import com.survivingwithandroid.weather.lib.request.WeatherRequest;
 
 public class FragmentWeather extends Fragment {
 
@@ -17,7 +27,41 @@ public class FragmentWeather extends Fragment {
         // Inflate the View
         View v = inflater.inflate(R.layout.fragment_weather, container, false);
 
+        final TextView textView2 = (TextView) v.findViewById(R.id.forecast);
+
         // Initiaizing Weather Client Builder
+        try {
+
+            WeatherClient client = (new WeatherClient.ClientBuilder()).attach(getActivity())
+                    .httpClient(WeatherDefaultClient.class)
+                    .provider(new OpenweathermapProviderType())
+                    .config(new WeatherConfig())
+                    .build();
+
+            client.getCurrentCondition(new WeatherRequest("3169070"), new WeatherClient.WeatherEventListener(){
+
+                @Override
+                public void onWeatherRetrieved(CurrentWeather currentWeather) {
+                    float currentTemp = currentWeather.weather.temperature.getTemp();
+                    textView2.setText("City ["+currentWeather.weather.location.getCity()+"] Current temp ["+currentTemp+"]");
+                }
+
+                @Override
+                public void onWeatherError(WeatherLibException e) {
+                    textView2.setText("Weather Error - parsing data");
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onConnectionError(Throwable throwable) {
+                    textView2.setText("Connection error");
+                    throwable.printStackTrace();
+                }
+            });
+
+        } catch (Throwable t){
+            t.printStackTrace();
+        }
         return v;
     }
 
